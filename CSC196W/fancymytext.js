6db6273f269546f9b603bb0f37d4137407df8f40
ddsooxo <dchuteng@gmail.com>
2021-02-20 23:49:57 -0800

    adding JavaScript assignment, almost complete

---
+++ CSC196W/fancymytext.js
@@ -0,0 +1,37 @@
+function bigger() {
+    document.getElementById("big-button");
+    document.getElementById("text-area").style.fontSize = "24pt";
+    // alert("hello world!");
+
+}
+
+
+
+function fancyText() {
+    document.getElementById("fancify");
+    document.getElementById("text-area").style.fontFamily = "cursive";
+    document.getElementById("text-area").style.fontWeight = "bold";
+    document.getElementById("text-area").style.color = "blue";
+    document.getElementById("text-area").style.backgroundColor = "beige";
+    document.getElementById("text-area").style.textDecoration = "underline";
+
+}
+
+function plainText() {
+    document.getElementById("plainJane");
+    document.getElementById("text-area").style.fontFamily = "inherit";
+    document.getElementById("text-area").style.fontWeight = "inherit";
+    document.getElementById("text-area").style.color = "inherit";
+    document.getElementById("text-area").style.backgroundColor = "inherit";
+    document.getElementById("text-area").style.textTransform = "inherit";
+}
+
+
+function moo() {
+    document.getElementById("moo-button");
+    document.getElementById("text-area").style.textTransform = "uppercase";
+}
+
+function alert() {
+    //alert("hello world!");
+}
